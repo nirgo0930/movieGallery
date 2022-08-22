15209d1e89ec5a23141b37ae644c3684386e7eed
xkakr <xkakrlfh@naver.com>
2022-08-22 10:18:14 +0900

    220822 signup

--- web/js/InputJS.js
+++ web/js/InputJS.js
@@ -1,6 +1,16 @@
-var flagID=false;//id중복체크 확인 여부
+var flagID = false;//id중복체크 확인 여부
 
-function checkUID(){
-    flagID=true;
-    open('openSb.jsp','openSb','width=400, height=140, left=100, top=200');
+function check() {
+    if (flagID == false) {
+        alert("ID 중복체크 하세요");
+        return;
+    }
+}
+
+function flagID(){ flagID=true; }
+
+
+function checkUID() {
+    flagID = true;
+    open('openSb.jsp', 'openSb', 'width=400, height=140, left=100, top=200');
 }
